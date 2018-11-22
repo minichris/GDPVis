dbd025b0f55f20bff5ca0ab473528d240d16841c
minichris <minichris502@gmail.com>
2018-11-22 01:49:12 +0000

    Moved browser parts to its own js file

---
+++ browser.js
@@ -0,0 +1,28 @@
+//function for creating a document browser
+function CreateDocumentViewer(pageTitle){
+	$("#DocumentViewer").html('<div class="insertedPage"></div>');
+	$("#DocumentViewer").addClass("HasInsertedPage");
+	$(".insertedPage").html(Patterns.find(pattern => pattern.Title == pageTitle).Content);
+	$("#DocumentViewer").scrollTop(0); //scroll it back to the top for the user
+	document.getElementById("DocumentViewer").scrollTop = 0;
+	$("#DocumentViewer").find("a[href]").click(function(e){
+		DocumentViewerEventHandler(e);
+	});
+}
+
+//function for handling link clicks in the document browser
+function DocumentViewerEventHandler(e){
+	//prevent the link from acutally working
+	e.stopPropagation();
+	e.preventDefault();
+	//get where the link was going to
+	var linkClicked = e.target.attributes['title'].value;
+	//get some new filters based on the selected link and update the filter list
+	Filters = generateReleventFilters(linkClicked);
+	refreshGraph(performFiltering(Patterns));
+	//check if the link click was a pattern that would result in a pattern in the node-link diagram being selected
+	if(checkPatternCurrentlyFiltered(linkClicked)){
+		ChangeSelection(linkClicked); //select the pattern
+	}
+	filterlistComponent.forceUpdate();
+}
--- graph.js
+++ graph.js
@@ -196,41 +196,13 @@ function generateGraph(data) {
 	}
 }
 
-
 $('#SearchSelect').change(function(){
 	if($('#SearchSelect').val() != undefined){
 		ChangeSelection($('#SearchSelect').val());
 	}
 });
 
-function CreateDocumentViewer(pageTitle){
-	$("#DocumentViewer").html('<div class="insertedPage"></div>');
-	$("#DocumentViewer").addClass("HasInsertedPage");
-	$(".insertedPage").html(Patterns.find(pattern => pattern.Title == pageTitle).Content);
-	$("#DocumentViewer").scrollTop(0); //scroll it back to the top for the user
-	document.getElementById("DocumentViewer").scrollTop = 0
-	$("#DocumentViewer").find("a[href]").click(function(e){
-		DocumentViewerEventHandler(e);
-	});
-}
-
-
-function DocumentViewerEventHandler(e){
-	//prevent the link from acutally working
-	e.stopPropagation();
-	e.preventDefault();
-	//get where the link was going to
-	var linkClicked = e.target.attributes['title'].value;
-	//get some new filters based on the selected link and update the filter list
-	Filters = generateReleventFilters(linkClicked);
-	refreshGraph(performFiltering(Patterns));
-	//check if the link click was a pattern that would result in a pattern in the node-link diagram being selected
-	if(checkPatternCurrentlyFiltered(linkClicked)){
-		ChangeSelection(linkClicked); //select the pattern
-	}
-	filterlistComponent.forceUpdate();
-}
-
+//function which handles changing the currently selected pattern
 function ChangeSelection(newSelectionID){
 	//handle the document viewer
 	CreateDocumentViewer(newSelectionID);
