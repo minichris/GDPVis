4232180b81ea55ccb9ea5041c74f9a907ac98dfd
minichris <minichris502@gmail.com>
2019-07-16 21:52:54 +0100

    Updated building stuff

--- build.bat
+++ build.bat
@@ -1,3 +1,3 @@
-copy "..\Parser\bin\Debug\netcoreapp2.0\AllGames.json" "build\AllGames.json"
-copy "..\Parser\bin\Debug\netcoreapp2.0\AllPatterns.json" "build\AllPatterns.json"
+copy "..\GDPWikiscrape\bin\Debug\netcoreapp2.0\AllGames.json" "build\AllGames.json"
+copy "..\GDPWikiscrape\bin\Debug\netcoreapp2.0\AllPatterns.json" "build\AllPatterns.json"
 npm run babel
