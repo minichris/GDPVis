0a73de9a5d1a76f8c72aac3c1a0806dba64963a7
Zaperox <admin@carolineleeceramics.co.uk>
2019-09-19 18:51:25 +0100

    Updated the home page a bit

--- src/browser/pagetypes/SpecialPage.js
+++ src/browser/pagetypes/SpecialPage.js
@@ -8,37 +8,22 @@ export default class SpecialPage extends React.Component {
                 <div className="insertedPage SpecialPage">
                     <h1>Welcome to VGTropes</h1>
                     <p>
-                    VGTropes is a visualization tool built on top of the GameDesignPatterns.org wiki.
-                    It can help you browse the collection of patterns, analysis games, contribute to the wiki, and much more.
-                    To get started, why not try looking at the <a href="javascript:void(0)" title="Category:Games">Games category</a> for some inspiration,
-                    or try filtering the patterns shown by clicking the "Filters" button in the graph view, or just clicking a node in the graph.
+						Hello and welcome to VGTropes. This is a visualization tool built to help you view the information found on the GameplayDesignPatterns.org wiki, with a major focus on allowing better understanding of the links found between patterns.
                     </p>
-                    <h2>Features</h2>
-        			<ul>
-        				<li>Clicking on a node will change the currently viewed article.</li>
-        				<li>Hovering over a node will show its name.</li>
-        				<li>The graph can be zoomed using the mouse wheel and can be panned by clicking and dragging the background.</li>
-        				<li>The nodes can be tugged about by click-dragging a node.</li>
-        				<li>Clicking the "Filters" button will display the currently enabled Filters.</li>
-        				<li>In the Filters menu, the Filters are appiled in order, top to bottom. There is currently no way to do "OR" logic.</li>
-        				<li>The Filters only update when you press "Apply Filters".</li>
-        				<li>Not applying enough filtering will cause a message to come up warning you that you are trying to display too many nodes. It is highly recomended you heed this warning.</li>
-                        <li>The visualization starts with example Filters which only show <b>patterns found in the game <i>"World of Warcraft"</i></b> and <b>patterns in the <i>Negative Patterns</i> category</b>.</li>
-                        <li>Hovering over the links between nodes shows a tooltip which explains the context of the link from the articles and any "relations" the articles have.</li>
-                        <li>The current Filters (but not the current page) are saved to the URL. Copy / bookmark the url to save your Filters.</li>
-                        <li>Game and Category pages are generated in-browser.</li>
-        			</ul>
-        			<h2>Planned</h2>
-        			<ul>
-        				<li>Currently, the nodes on the graph have random colours and links have random strengths. At some point these will be given meaning.</li>
-                        <li>The currently appiled Filters should be shown at the bottom of the graph in a human readable format.</li>
-                        <li>NOT gates, OR gates and multi-selects should be added to filtering.</li>
-                        <li>Clicking on nodes should "magnetise" connected nodes around it.</li>
-                        <li>A "Creator mode".</li>
-                        <li>Make zoom configurable to not zoom, but to weaken gravity.</li>
-                        <li>Display the amount of currently loaded Patterns and Links somewhere.</li>
-        				<li>Working title!</li>
-        			</ul>
+                    <h2>How do I use it?</h2>
+						There are three main ways to use VGTropes...
+        			<h3>Filtering</h3>
+						Write about filtering
+        			<h3>Browsing</h3>
+						Write about browsing
+        			<h3>Searching</h3>
+						Know what you are looking for already? The search bar in the top left will allow you to quickly jump to any Pattern, Game, Pattern category or Game category on the wiki. When you press enter, not only will it bring up this page, but it will also set up the filters to display a graph of related patterns. For example, selecting a pattern page from your search will create a graph of all the patterns relating to the selected pattern. Selecting a game page will show all the patterns which are linked to that game. Selecting category pages will fill the graph will patterns or games from that category. It can also search within the content of a pattern page, and from that will create a graph of patterns that contain those words.
+        			<h3>Other Features</h3>
+						Write about the ToC, links, etc
+        			<h2>Q&A</h2>
+        			<i>Some pages look different to the original wiki, why is that?</i>
+					<br />
+						The game pages and category pages you find on this system are generated from the patterns in this system, rather then using the wiki’s own pages. Also, the pattern information in this system is only updated every so often, which means any recent changes on the wiki will take some time to propagate to this visualization. Don’t worry, edits to the wiki will get here eventually!
                 </div>
             );
         }
