bd58366829762a566b427b772c03043df8b036ab
minichris <minichris502@gmail.com>
2019-09-20 20:35:36 +0100

    Wrote some more on the default page

--- src/browser/pagetypes/SpecialPage.js
+++ src/browser/pagetypes/SpecialPage.js
@@ -13,7 +13,19 @@ export default class SpecialPage extends React.Component {
                     <h2>How do I use it?</h2>
 						There are three main ways to use VGTropes...
         			<h3>Filtering</h3>
-						Write about filtering
+						VGTropes has an experimental filtering system inspired by Unreal Engine 4's blueprint programming system. It uses the <a target="_blank" href="http://rete.js.org">rete.js</a> library to create the filtering graphs. <br />
+						The reasoning for a somewhat more complex than usual filtering system is it is it allows the end-user (a.k.a you) a modular system of filtering for both the games and patterns found on the wiki, which allows a finer control of the output than a more standard filter setup. <br />
+						<ul>
+							<li>You can open the filtering system by clicking the "Change Filters" button in the bottom left, which only appears when the document viewer (this panel) is closed. </li>
+							<li>When the panel is open, right clicking the background will open a menu which contains a list of all of the filtering nodes in the system.</li>
+							<li>Purple connectors are arrays (lists) of patterns, and green connectors are arrays of games. Gray connections accept either an array of games or an array of patterns, but keep in mind that games and patterns can not be displayed at the same time. </li>
+							<li>You can only have one <i>working</i> output node. </li> 
+							<li>A node's output connections can be plugged into many nodes inputs, but one input can only accept one connection.</li>
+							<li>If you need to merge arrays, check out the "Array Union", "Array Intersection" and "Array Difference" nodes (these can be thought of as "lets through if in either", "lets through if in both" and "lets through those from A if not in B").</li>
+							<li>The graph won't update while nothing is plugged into the output node.</li>
+							<li>Right clicking a node will allow you to clone the node or delete it. You can also delete by pressing the delete key.</li>
+							<li>Don't know where to start? Try searching something, which will generate a set of example filter nodes.</li>
+						</ul>
 					<h4>Example Filters</h4>
 						Why not check out some of these patterns for insperation?
 						<ul>
