fd433c4296f4d8821a6270d0eced9e86336051f0
Zaperox <admin@carolineleeceramics.co.uk>
2019-11-04 17:06:21 +0000

    Participant ID is now entered through the get request

--- src/logger.js
+++ src/logger.js
@@ -1,4 +1,5 @@
 import $ from 'jquery';
+import {decodeJSONfromString} from './history.js';
 
 const logURL = "http://10.5.35.35:25564/logs";
 
@@ -9,11 +10,21 @@ export function startLogger(){
 	$.ajax({
 		url: logURL,
 		success: function(){
-			let participantID = window.prompt("Please enter your participant ID");
+			var urlParams = new URLSearchParams( new URL(window.location).search);
+			
+			let participantID;
+			if(urlParams.has('participantid')) { //if the url has Filters in the GET request
+				participantID = urlParams.get('participantid');
+			}
+			else{
+				return null;
+			}
+			
 			Logger.setHandler(function (messages, context) {
 				let time = Math.round((new Date()).getTime() / 1000);
 				$.post(logURL, { timestamp: time, participant: participantID, message: messages[0], level: context.level });
 			});
+			
 		}
 	});
 }
