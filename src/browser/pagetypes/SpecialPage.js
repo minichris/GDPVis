4ecfd1f84c838dbdc1380ce76b505141779cdd29
Zaperox <admin@carolineleeceramics.co.uk>
2019-11-12 16:03:08 +0000

    Added more detail to the homepage

--- src/browser/pagetypes/SpecialPage.js
+++ src/browser/pagetypes/SpecialPage.js
@@ -14,32 +14,34 @@ export default class SpecialPage extends React.Component {
 						There are three main ways to use GDPVis...
         			<details title="Click to expand">
 						<summary><h3>Filtering</h3></summary>
-							GDPVis has an experimental filtering system inspired by Unreal Engine 4's blueprint programming system. It uses the <a target="_blank" href="http://rete.js.org">rete.js</a> library to create the filtering graphs. <br />
-							The reasoning for a somewhat more complex than usual filtering system is it is it allows the end-user (a.k.a you) a modular system of filtering for both the games and patterns found on the wiki, which allows a finer control of the output than a more standard filter setup. <br />
-							<ul>
-								<li>You can open the filtering system by clicking the "Change Filters" button in the bottom left, which only appears when the document viewer (this panel) is closed. </li>
-								<li>When the panel is open, right clicking the background will open a menu which contains a list of all of the filtering nodes in the system.</li>
-								<li>Purple connectors are arrays (lists) of patterns, and green connectors are arrays of games. Gray connections accept either an array of games or an array of patterns, but keep in mind that games and patterns can not be displayed at the same time. </li>
-								<li>You can only have one <i>working</i> output node. </li> 
-								<li>A node's output connections can be plugged into many nodes inputs, but one input can only accept one connection.</li>
-								<li>If you need to merge arrays, check out the "Array Union", "Array Intersection" and "Array Difference" nodes (these can be thought of as "lets through if in either", "lets through if in both" and "lets through those from A if not in B").</li>
-								<li>The graph won't update while nothing is plugged into the output node.</li>
-								<li>Right clicking a node will allow you to clone the node or delete it. You can also delete by pressing the delete key.</li>
-								<li>Don't know where to start? Try searching something, which will generate a set of example filter nodes.</li>
-							</ul>
-						<h4>Example Filters</h4>
-							Why not check out some of these patterns for insperation?
-							<ul>
-							<li><a href="javascript:void(0)" title="Category:Games">
-							All Games
-							</a></li>
-							<li><a href="javascript:void(0)" title="Game World Exploration">
-							All patterns relating to Game World Exploration
-							</a></li>
-							<li><a href="javascript:void(0)" title="Category:FPS Games">
-							All games in the category "FPS Games"
-							</a></li>
-							</ul>
+						GDPVis has an experimental filtering system inspired by Unreal Engine 4's blueprint programming system. It uses the <a target="_blank" href="http://rete.js.org">rete.js</a> library to create the filtering graphs. <br />
+						The reasoning for a somewhat more complex than usual filtering system is it is it allows the end-user (a.k.a you) a modular system of filtering for both the games and patterns found on the wiki, which allows a finer control of the output than a more standard filter setup. <br />
+						<ul>
+							<li>You can open the filtering system by clicking the "Change Filters" button in the bottom left, which only appears when the document viewer (this panel) is closed. </li>
+							<li>When the panel is open, right clicking the background will open a menu which contains a list of all of the filtering nodes in the system.</li>
+							<li>Purple connectors are arrays (lists) of patterns, and green connectors are arrays of games. Gray connections accept either an array of games or an array of patterns, but keep in mind that games and patterns can not be displayed at the same time. </li>
+							<li>You can only have one <i>working</i> output node. </li> 
+							<li>A node's output connections can be plugged into many nodes inputs, but one input can only accept one connection.</li>
+							<li>If you need to merge arrays, check out the "Array Union", "Array Intersection" and "Array Difference" nodes (these can be thought of as "lets through if in either", "lets through if in both" and "lets through those from A if not in B").</li>
+							<li>The graph won't update while nothing is plugged into the output node.</li>
+							<li>Right clicking a node will allow you to clone the node or delete it. You can also delete by pressing the delete key.</li>
+							<li>Don't know where to start? Try searching something, which will generate a set of example filter nodes.</li>
+						</ul>
+					</details>	
+					<details title="Click to expand" open>
+						<summary><h3>Example Filters</h3></summary>
+						Why not check out some of these patterns for insperation?
+						<ul>
+						<li><a href="javascript:void(0)" title="Category:Games">
+						All Games
+						</a></li>
+						<li><a href="javascript:void(0)" title="Game World Exploration">
+						All patterns relating to Game World Exploration
+						</a></li>
+						<li><a href="javascript:void(0)" title="Category:FPS Games">
+						All games in the category "FPS Games"
+						</a></li>
+						</ul>
 					</details>
         			<details title="Click to expand">
 						<summary><h3>Browsing</h3></summary>
@@ -51,7 +53,13 @@ export default class SpecialPage extends React.Component {
         			</details>
 					<details title="Click to expand">
 						<summary><h3>Other Features</h3></summary>
-						<p style={{color: "red"}}>Write about the ToC, links, etc</p>
+						The button labeled "ToC" opens the table of contents for the article you are currently looking at.<br />
+						Hovering over game/pattern nodes or pattern links in the node-link graph to open a tooltip about that game/pattern or relationship.
+						You can click on the links to find out more details about the relationship.
+						<br />
+						You can turn off one or more of the relationships in the "Relationships to Show" box.
+						<br />
+						You can find links to the original wiki in the top bar. 
 					</details>
         			<details title="Click to expand">
 						<summary><h3>Q&A</h3></summary>
